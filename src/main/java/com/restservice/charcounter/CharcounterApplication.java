d0918f76dcc0b52f9ccbf4120ef98e55d6024819
Lyfyfision <103595446+Lyfyfision@users.noreply.github.com>
2023-11-27 23:57:42 +0300

    Initial commit

---
+++ src/main/java/com/restservice/charcounter/CharcounterApplication.java
@@ -0,0 +1,13 @@
+package com.restservice.charcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharcounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharcounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restservice/charcounter/CharcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restservice.charcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharcounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
