8cb60059561cbdab17b5c52ffeddfa81a1a9a164
Lyfyfision <103595446+Lyfyfision@users.noreply.github.com>
2023-11-30 18:06:24 +0300

    Added openApi support

--- src/main/java/com/restservice/charcounter/web/CounterController.java
+++ src/main/java/com/restservice/charcounter/web/CounterController.java
@@ -2,8 +2,12 @@ package com.restservice.charcounter.web;
 
 import com.restservice.charcounter.io.Word;
 import com.restservice.charcounter.service.CounterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
@@ -19,6 +23,13 @@ public class CounterController {
     }
 
     @PostMapping("/letter-count")
+    @Operation(summary = "Get a string and find all occurrences of letters in it")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All occurrences found and sorted",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Word.class))}),
+            @ApiResponse(responseCode = "400", description = "String is empty/null or without letters",
+            content = @Content)
+    })
     public Map <Character, Integer> countCharacters(@RequestBody @Valid Word word) {
         Map<Character, Integer> unsorted = counterService.countLetters(word.getInput());
         return counterService.sortMapOfLetters(unsorted);
